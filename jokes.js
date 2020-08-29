05e738ea2bfcc7ad8c5dd5f2c0462e91904b65ee
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-08-29 06:41:14 +0100

    Updated

--- jokes.js
+++ jokes.js
@@ -1,3 +1,6 @@
+const jokeBtn = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+
 const buttonText = [
 	'Ugh.',
 	'🤦🏻‍♂️',
@@ -8,3 +11,31 @@ const buttonText = [
 	'please stop',
 	'that was the worst one',
 ];
+
+function randomItemFromArr(arr, not) {
+	const item = arr[Math.floor(Math.random() * arr.length)];
+	if (item === not) {
+		return randomItemFromArr(arr, not);
+	}
+	return item
+}
+
+async function fetchJoke() {
+	const res = await fetch('https://icanhazdadjoke.com', {
+		headers: {
+			Accept: 'application/json',
+		}
+	});	
+	const data = await res.json();
+	return data;
+}
+fetchJoke();
+
+async function handleClick() {
+	const { joke } = await fetchJoke();
+	jokeHolder.textContent = joke;
+
+	jokeBtn.textContent = randomItemFromArr(buttonText, jokeBtn.textContent);
+}
+
+jokeBtn.addEventListener('click', handleClick);
