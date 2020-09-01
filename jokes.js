e0180306915b1cc5d39e8fe7f0d9c368ea2ae4a6
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-09-01 01:29:08 +0100

    Refactored

--- jokes.js
+++ jokes.js
@@ -1,41 +1,5 @@
-const jokeBtn = document.querySelector('.getJoke');
-const jokeHolder = document.querySelector('.joke p');
-
-const buttonText = [
-	'Ugh.',
-	'🤦🏻‍♂️',
-	'omg dad.',
-	'you are the worst',
-	'seriously',
-	'stop it.',
-	'please stop',
-	'that was the worst one',
-];
-
-function randomItemFromArr(arr, not) {
-	const item = arr[Math.floor(Math.random() * arr.length)];
-	if (item === not) {
-		return randomItemFromArr(arr, not);
-	}
-	return item
-}
-
-async function fetchJoke() {
-	const res = await fetch('https://icanhazdadjoke.com', {
-		headers: {
-			Accept: 'application/json',
-		}
-	});	
-	const data = await res.json();
-	return data;
-}
-fetchJoke();
-
-async function handleClick() {
-	const { joke } = await fetchJoke();
-	jokeHolder.textContent = joke;
-
-	jokeBtn.textContent = randomItemFromArr(buttonText, jokeBtn.textContent);
-}
+import { handleClick } from './lib/handlers.js';
+import { jokeBtn } from './lib/elements.js';
+// fetchJoke();
 
 jokeBtn.addEventListener('click', handleClick);
