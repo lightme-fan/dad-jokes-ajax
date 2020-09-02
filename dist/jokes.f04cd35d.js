09347b5691fd3dc1419c480184f118f0c94de8fd
Fanilo Andriamarohaja <fanilo.and@onja.org>
2020-09-02 07:01:24 +0100

    added module and parcel

---
+++ dist/jokes.f04cd35d.js
@@ -0,0 +1,14 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"bRhr":[function(require,module,exports) {
+"use strict";async function e(){const e=await fetch("https://icanhazdadjoke.com",{headers:{Accept:"application/json"}});return await e.json()}Object.defineProperty(exports,"__esModule",{value:!0}),exports.fetchJoke=e;
+},{}],"FJv4":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.jokeHolder=exports.jokeBtn=void 0;const e=document.querySelector(".getJoke");exports.jokeBtn=e;const o=document.querySelector(".joke p");exports.jokeHolder=o;
+},{}],"UOOq":[function(require,module,exports) {
+"use strict";function e(r,t){const o=r[Math.floor(Math.random()*r.length)];return o===t?e(r,t):o}Object.defineProperty(exports,"__esModule",{value:!0}),exports.randomItemFromArr=e;
+},{}],"gfqm":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;const e=["Ugh.","🤦🏻‍♂️","omg dad.","you are the worst","seriously","stop it.","please stop","that was the worst one"];var t=e;exports.default=t;
+},{}],"Dqfr":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.handleClick=u;var e=require("./index.js"),t=require("./elements.js"),n=require("./utils.js"),r=o(require("../data/buttonText.js"));function o(e){return e&&e.__esModule?e:{default:e}}async function u(){const{joke:o}=await(0,e.fetchJoke)();t.jokeHolder.textContent=o,t.jokeBtn.textContent=(0,n.randomItemFromArr)(r.default,t.jokeBtn.textContent)}
+},{"./index.js":"bRhr","./elements.js":"FJv4","./utils.js":"UOOq","../data/buttonText.js":"gfqm"}],"Wmte":[function(require,module,exports) {
+"use strict";var e=require("./lib/handlers.js"),i=require("./lib/elements.js");i.jokeBtn.addEventListener("click",e.handleClick);
+},{"./lib/handlers.js":"Dqfr","./lib/elements.js":"FJv4"}]},{},["Wmte"], null)
+//# sourceMappingURL=/jokes.f04cd35d.js.map
